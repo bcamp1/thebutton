aa8041ce4e9fa67eb91768b8dfab102d010d2c21
Gage Hundertmark <hundertmarkgage@gmail.com>
2021-07-04 17:02:16 -0600

    added click limit

--- public/frontend.js
+++ public/frontend.js
@@ -3,9 +3,7 @@ var socket = io()
 let count = 0;
 
 document.getElementById("button").onclick = () => {
-    count += 1;
     socket.emit('pressed')
-    document.getElementById("number").textContent = count.toString()
 }
 
 socket.on('count', (updatedCount) => {
