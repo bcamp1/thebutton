3479d679ef0dbff9390aef98d8ce092eb1680e0c
Branson Camp <bransoncamp@outlook.com>
2021-07-05 00:23:41 -0600

    added file backup

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server)
 const port = 8080
+const fs = require('fs')
+
+const backupInterval = 10_000
 
 app.use(express.static("public"))
 
 let users = 0
 let count = 0
+
+fs.readFile("count.txt", "utf-8", (err, data) => {
+    if (err) { 
+        count = 0
+    } else {
+        count = parseInt(data)
+    }
+})
 
 io.on('connection', (socket) => {
     socket.lastCount = 0;
@@ -36,3 +47,13 @@ io.on('connection', (socket) => {
 
 server.listen(port)
 console.log('Listening on port ' + port + '...')
+
+// File backup loop
+function backupCount() {
+    fs.writeFile("count.txt", count.toString(), (err) => {
+        if (err) console.log(err)
+        console.log("Backup successful")
+    })
+}
+
+setInterval(backupCount, backupInterval)
